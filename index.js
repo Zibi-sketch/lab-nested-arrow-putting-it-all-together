9e652350ce3f09aab683edac9c1bc1d27fb11705
ZizaKariuki <zizawabetti@gmail.com>
2026-04-06 15:15:54 +0300

    Attempt_3

--- index.js
+++ index.js
@@ -3,40 +3,28 @@ let correctPassword = userInfo.password;
 let failedAttempts = 0;
 let locked = false;
 
-  return (passwordAttempt) => {
+  return function(passwordAttempt) {
 
     if(locked){
-      return "Account is locked. No more login attempts allowed.";
+      return "Account is locked.";
     }
 
-    if (passwordAttempt == correctPassword) {
+    if (passwordAttempt === correctPassword) {
       failedAttempts = 0;
       return "Login successful";
       
     }
 
-    else{
       failedAttempts ++;
-    }
 
     if (failedAttempts >= 3){
       locked = true;
-      return "Account locked due to too many failed login attempts";
+      return "Account locked";
     }
-    return "Login failed\nLogin attempt number: " + (failedAttempts) + " out of 3.";
+    return "Login failed. Login attempt number: " + (failedAttempts);
   }
 }
 
-let userInput = {
-  username: "Ziza",
-  password: "Pinkflamingo",
-}
-
-let login = createLoginTracker(userInput);
-
-console.log (login(userInput));
-console.log (login(userInput));
-console.log (login(userInput));
-console.log (login(userInput));
 
 module.exports = createLoginTracker;
+
